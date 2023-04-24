a1234ece0cbb153d12453bf79e6b265b9f7f5b69
Jovann Contreras <jcontreras@codestack.co>
2023-04-24 10:18:00 -0700

    Created file structure of folder we need right now

---
+++ Services/Context/DataContext.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace taskTrackerBackend.Services.Context
+{
+    public class DataContext
+    {
+        
+    }
+}
